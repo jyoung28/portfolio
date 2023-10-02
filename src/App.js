26ae78b4991c751bb5bb4d3fcd6968911751aaed
Jacob Young <youngjacob2002@gmail.com>
2023-10-01 19:14:53 -0700

    hashrhoute

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {HashRouter as Router, Route, Routes} from 'react-router-dom'
 import TimelinePage from './Pages/TimeLinePage';
 import Header from './Components/Header';
 import LandingPage from './Pages/LandingPage';
