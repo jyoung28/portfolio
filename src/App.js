f1159f28ef9105ba4502171e3b006b6ea7bcab19
Jacob Young <youngjacob2002@gmail.com>
2023-10-02 12:13:38 -0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {HashRouter as Router, Route, Routes} from 'react-router-dom'
+import {HashRouter, BrowserRouter, Route, Routes} from 'react-router-dom'
 import TimelinePage from './Pages/TimeLinePage';
 import Header from './Components/Header';
 import LandingPage from './Pages/LandingPage';
@@ -9,17 +9,18 @@ import ContactMe from './Pages/ContactMe';
 function App() {
   return (
     <div className="App">
-      
-      <Router>
-          {/* have the header only be on pages after login */}
-          <Header/>
-          <Routes>
-              <Route element={<LandingPage/>} path="/#/" exact/>
-              <Route element={<TimelinePage/>} path="/#/timeline" exact/>
-              <Route element={<Projects/>} path="/#/projects" exact/>
-              <Route element={<ContactMe/>} path="/#/contactme" exact/>
-          </Routes>
-      </Router>
+      <BrowserRouter>
+        <HashRouter basename='/'>
+            {/* have the header only be on pages after login */}
+            <Header/>
+            <Routes>
+                <Route element={<LandingPage/>} path="/" exact/>
+                <Route element={<TimelinePage/>} path="/timeline" exact/>
+                <Route element={<Projects/>} path="/projects" exact/>
+                <Route element={<ContactMe/>} path="/contactme" exact/>
+            </Routes>
+        </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
